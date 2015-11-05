f3de25abc4f32fb78fc2478495dcf1741b5a7557
Sergey Laptev <iamsergeylaptev@gmail.com>
2015-11-05 22:10:18 +0300

    Make react-dom external and fix example

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,20 @@
 var webpack = require('webpack');
 var path = require('path');
 
+var reactExternal = {
+  root: 'React',
+  commonjs2: 'react',
+  commonjs: 'react',
+  amd: 'react',
+};
+
+var reactDOMExternal = {
+  root: 'ReactDOM',
+  commonjs2: 'react-dom',
+  commonjs: 'react-dom',
+  amd: 'react-dom',
+}
+
 module.exports = {
   entry: ['./src/LazyLoad.jsx'],
   output: {
@@ -19,18 +33,12 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx'],
   },
-  externals: [
-    {
-      react: {
-        root: 'React',
-        commonjs2: 'react',
-        commonjs: 'react',
-        amd: 'react'
-      }
-    }
-  ],
+  externals: {
+    'react': reactExternal,
+    'react-dom': reactDOMExternal,
+  },
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
