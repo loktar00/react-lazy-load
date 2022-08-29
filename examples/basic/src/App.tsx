ab3ca9cf96e32c120a43c9f36b3a519bc0707da0
Jason <loktar69@hotmail.com>
2022-08-28 21:48:42 -0400

    Converted to use intersectionObeserver, removed findDom, updated example

--- examples/basic/src/App.tsx
+++ examples/basic/src/App.tsx
@@ -4,31 +4,31 @@ import LazyLoad from 'react-lazy-load';
 import './style.css';
 
 const App = () => (
-    <div>
-        Scroll to load images.
-        <div className="filler" />
-        <LazyLoad height={762} offsetVertical={300}>
+    <main className="main-container">
+        <h1>Basic Examples, Scroll to load images.</h1>
+        <LazyLoad width={1024} height={762} threshold={0.99}>
             <img src="http://apod.nasa.gov/apod/image/1502/HDR_MVMQ20Feb2015ouellet1024.jpg" alt="feb 20" />
         </LazyLoad>
-        <div className="filler" />
-        <LazyLoad height={683} offsetVertical={300}>
+        <LazyLoad height={683}>
             <img src="http://apod.nasa.gov/apod/image/1502/2015_02_20_conj_bourque1024.jpg" alt="2015" />
         </LazyLoad>
-        <div className="filler" />
+        <h2>Testing within an internal container</h2>
         <div className="scrollable-container">
-            <div className="filler" />
-            <div className="filler" />
-            <div className="filler" />
             <LazyLoad height={480}>
                 <img src="http://apod.nasa.gov/apod/image/1502/MarsPlume_jaeschke_480.gif" alt="Mars Plume" />
             </LazyLoad>
         </div>
-        <div className="filler" />
-        <LazyLoad height={720} offsetVertical={300}>
+        <h2>Offset of 300</h2>
+        <LazyLoad height={720} offset={300}>
             <img src="http://apod.nasa.gov/apod/image/1502/ToadSky_Lane_1080_annotated.jpg" alt="Toad SKy" />
         </LazyLoad>
-        <div className="filler" />
-    </div>
+        <h2>Horizontal Test, Sroll to the right</h2>
+        <div className="horizontal-scroll">
+            <LazyLoad height={683}>
+                <img src="http://apod.nasa.gov/apod/image/1502/2015_02_20_conj_bourque1024.jpg" alt="Mars Plume" />
+            </LazyLoad>
+        </div>
+    </main>
 );
 
 export default App;
--- src/LazyLoad.tsx
+++ src/LazyLoad.tsx
@@ -2,29 +2,19 @@ import React, {
     Children,
     Component,
     createElement,
-    ReactNode
+    ReactNode,
+    RefObject
 } from 'react';
-import { findDOMNode } from 'react-dom';
-import _debounce from 'lodash/debounce';
-import _throttle from 'lodash/throttle';
 // eslint-disable-next-line import/extensions
-import { scrollParent, inViewport } from './utils/index.js';
+import scrollParent from './utils/index.js';
 
 type Props = {
   children: ReactNode,
   className?: string,
-  debounce?: boolean,
   elementType?: string,
   height?: string | number,
-  offset?: number,
-  offsetBottom?: number,
-  offsetHorizontal?: number,
-  offsetLeft?: number,
-  offsetRight?: number,
-  offsetTop?: number,
-  offsetVertical?: number,
+  offset?: string | number,
   threshold?: number,
-  throttle?: number,
   width?: number | string,
   onContentVisible?: () => void,
 }
@@ -37,55 +27,44 @@ export default class LazyLoad extends Component<Props, State> {
     static defaultProps = {
         elementType: 'div',
         className: '',
-        debounce: false,
         offset: 0,
-        offsetBottom: 0,
-        offsetHorizontal: 0,
-        offsetLeft: 0,
-        offsetRight: 0,
-        offsetTop: 0,
-        offsetVertical: 0,
-        throttle: 250,
         threshold: 0,
         width: null,
         onContentVisible: null,
         height: null
     };
 
-    mounted: boolean;
+    elementObserver: IntersectionObserver | null;
 
-    constructor({ throttle, debounce, ...props }: Props) {
+    wrapper: RefObject<Component> | null;
+
+    constructor(props: Props) {
         super(props);
-
-        this.lazyLoadHandler = this.lazyLoadHandler.bind(this);
-        this.mounted = false;
-
-        if (throttle) {
-            if (debounce) {
-                this.lazyLoadHandler = _debounce(this.lazyLoadHandler, throttle);
-            } else {
-                this.lazyLoadHandler = _throttle(this.lazyLoadHandler, throttle);
-            }
-        }
+        this.elementObserver = null;
+        this.wrapper = React.createRef();
 
         this.state = { visible: false };
     }
 
     componentDidMount() {
-        this.mounted = true;
-        const eventNode = this.getEventNode();
+        let eventNode = this.getEventNode();
 
-        this.lazyLoadHandler();
-
-        if (this.lazyLoadHandler.flush) {
-            this.lazyLoadHandler.flush();
+        if (eventNode === window) {
+            eventNode = document.body;
         }
+        const { offset, threshold } = this.props;
 
-        window.addEventListener('resize', this.lazyLoadHandler);
-        eventNode.addEventListener('scroll', this.lazyLoadHandler);
+        const options = {
+            rootMargin: typeof offset === 'number' ? `${offset}px` : offset || '0px',
+            threshold: threshold || 0
+        };
 
-        if (eventNode !== window) {
-            window.addEventListener('scroll', this.lazyLoadHandler);
+        this.elementObserver = new IntersectionObserver(this.lazyLoadHandler, options);
+
+        const node = this.wrapper?.current;
+
+        if (node instanceof HTMLElement) {
+            this.elementObserver.observe(node);
         }
     }
 
@@ -93,76 +72,36 @@ export default class LazyLoad extends Component<Props, State> {
         return nextState.visible;
     }
 
-    componentDidUpdate() {
-        const { visible } = this.state;
-
-        if (!visible) {
-            this.lazyLoadHandler();
-        }
-    }
-
     componentWillUnmount() {
-        this.mounted = false;
-
-        if (this.lazyLoadHandler.cancel) {
-            this.lazyLoadHandler.cancel();
+        const node = this.wrapper?.current;
+        if (node && node instanceof HTMLElement) {
+            this.elementObserver?.unobserve(node);
         }
-
-        this.detachListeners();
     }
 
     getEventNode() {
-        return scrollParent(findDOMNode(this));
+        return scrollParent(this.wrapper?.current);
     }
 
-    getOffset() {
-        const {
-            offset, offsetVertical, offsetHorizontal,
-            offsetTop, offsetBottom, offsetLeft, offsetRight, threshold
-        } = this.props;
+    lazyLoadHandler = (entries: IntersectionObserverEntry[]) => {
+        const { onContentVisible } = this.props;
+        const [entry] = entries;
+        const { isIntersecting } = entry;
 
-        const offsetAll = threshold || offset;
-        const verticleOffset = offsetVertical || offsetAll;
-        const horizontalOffset = offsetHorizontal || offsetAll;
-
-        return {
-            top: offsetTop || verticleOffset,
-            bottom: offsetBottom || verticleOffset,
-            left: offsetLeft || horizontalOffset,
-            right: offsetRight || horizontalOffset
-        };
-    }
-
-    lazyLoadHandler() {
-        if (!this.mounted) {
-            return;
-        }
-        const offset = this.getOffset();
-        const node = findDOMNode(this);
-        const eventNode = this.getEventNode();
-
-        if (inViewport(node, eventNode, offset)) {
-            const { onContentVisible } = this.props;
-
+        if (isIntersecting) {
             this.setState({ visible: true }, () => {
                 if (onContentVisible) {
                     onContentVisible();
                 }
             });
-            this.detachListeners();
+
+            // Stop observing
+            const node = this.wrapper?.current;
+            if (node && node instanceof HTMLElement) {
+                this.elementObserver?.unobserve(node);
+            }
         }
-    }
-
-    detachListeners() {
-        const eventNode = this.getEventNode();
-
-        window.removeEventListener('resize', this.lazyLoadHandler);
-        eventNode.removeEventListener('scroll', this.lazyLoadHandler);
-
-        if (eventNode !== window) {
-            window.removeEventListener('scroll', this.lazyLoadHandler);
-        }
-    }
+    };
 
     render() {
         const {
@@ -181,7 +120,8 @@ export default class LazyLoad extends Component<Props, State> {
 
         return createElement(componentElementType, {
             className: elClasses,
-            style: elStyles
+            style: elStyles,
+            ref: this.wrapper
         }, visible && Children.only(children));
     }
 }
